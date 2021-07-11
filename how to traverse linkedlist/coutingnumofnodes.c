4e11eccb13f28dfad2ae5e76c493c1a069eb192d
Surajkumar20 <srajarat@purdue.edu>
2021-07-11 11:43:28 -0400

    made functions for creating and appending nodes to a linked list

--- how to traverse linkedlist/coutingnumofnodes.c
+++ how to traverse linkedlist/coutingnumofnodes.c
@@ -1,10 +1,34 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+Node* createNode(int);
+Node* appendNode(Node*, Node*);
+
 struct node {
     int val;
     struct node* nextptr;
 };
+typedef struct node Node;
 
-// Function that adds node to an existing node
+// Function that returns the pointer of the newly, created node
+Node* createNode(int a) {
+    Node* nodee;
+    nodee = (Node*) malloc(sizeof(Node));
+    nodee->val = a;
+    nodee->nextptr = NULL;
+    return(nodee);
+}
 
+// Function that appends a node to a singly linked list
+Node* appendNode(Node* addedNode, Node* headList) {
+    Node* copy = headList;
+    
+    //Traverse through the linked list
+    while (headList != NULL) {
+        headList = headList->nextptr;
+    }
+
+    // Set the final node to point to the addedNode
+    headList->nextptr = addedNode;
+    return copy;
+}
