5fb275eb7b9f9b60800ee27fba44ec23188e2eea
Surajkumar20 <srajarat@purdue.edu>
2021-07-16 01:00:20 -0400

    Proper Ros Code

--- ROS/firstRos/puhlisher.py
+++ ROS/firstRos/puhlisher.py
@@ -1,11 +1,12 @@
-import ros 
-
-def createNode():
-    init_node("I think this is really cool")
-    loginfo("Begin Node you fucking bitch")
-    sleep(1)
-    loginfo("Exit now")
-
-if __name__ == "__main__":
-    new_func(createNode)
-    print("We made this work")
+#!/usr/bin/env python
+import rospy
+from std_msgs.msg import String
+   
+pub = rospy.Publisher('chatter', String, queue_size=10)
+rospy.init_node('talker', anonymous=True)
+rate = rospy.Rate(10) # 10hz
+while not rospy.is_shutdown():
+   hello_str = "hello world %s" % rospy.get_time()
+   rospy.loginfo(hello_str)
+   pub.publish(hello_str)
+   rate.sleep()
