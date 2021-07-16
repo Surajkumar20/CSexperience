e84934bd1b0010b95c2da1d7be677da4d6947391
Surajkumar20 <srajarat@DESKTOP-V9F4B6B.localdomain>
2021-07-15 22:53:12 -0400

    Just backing up my ROS

---
+++ ROS/firstRos/puhlisher.py
@@ -0,0 +1,11 @@
+import ros 
+
+def createNode():
+    init_node("I think this is really cool")
+    loginfo("Begin Node you fucking bitch")
+    sleep(1)
+    loginfo("Exit now")
+
+if __name__ == "__main__":
+    createNode()
+    print("We made this work")
---
+++ ROS/ranDom/yo.py
@@ -0,0 +1,9 @@
+print("I am going to use VIM for the rest of my life")
+i = 0
+while i !=  25:
+    i = i+1
+    print(i)
+    print("\n")
+
+print("That while loop worked easily")
+print("They really thinking they out here")
