d0d878c1769e383715ed02a8b02df81f3724fe5c
Surajkumar20 <srajarat@purdue.edu>
2021-08-04 22:17:20 -0400

    Got the \r stuff working

--- How Python Works/BackSlashR.py
+++ How Python Works/BackSlashR.py
@@ -1,9 +1,8 @@
 import time as t
-i = 0
 
-print("\n")
-while (i < 500):
+i = 0
+while (i < 100):
     i = i + 1
-    #print("This is the value of i:",i," \r")
-    print("\rI like this")
-    t.sleep(0.1)
+    print("This is the value of i: {}\r".format(i), end = "\r")
+    t.sleep(0.5)
+
